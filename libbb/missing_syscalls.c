29ab5cadf7eff5386cd13a8c63adb3087c7d69c1
Chris Renshaw <osm0sis@outlook.com>
2016-09-12 14:45:35 -0300

    android syscalls: shmget/msgget/semget
    Patch modified by Tias Guns <tias@ulyssis.org> from 'no-sys-shm,msg,sem' by Dan Drown
    http://dan.drown.org/android/src/busybox/
    Rebased for busybox 1.25.0 by Chris Renshaw <osm0sis@outlook.com>

--- libbb/missing_syscalls.c
+++ libbb/missing_syscalls.c
@@ -31,6 +31,21 @@ int pivot_root(const char *new_root, const char *put_old)
 	return syscall(__NR_pivot_root, new_root, put_old);
 }
 
+int shmget(key_t key, size_t size, int shmflg)
+{
+  return syscall(__NR_shmget, key, size, shmflg);
+}
+
+int msgget(key_t key, int msgflg)
+{
+  return syscall(__NR_msgget, key, msgflg);
+}
+
+int semget(key_t key, int nsems, int semflg)
+{
+  return syscall(__NR_semget, key, nsems, semflg);
+}
+
 # if __ANDROID_API__ < 21
 int tcdrain(int fd)
 {
