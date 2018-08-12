5aa32ead482e6668627356f78f92ddd08a97aea9
topjohnwu <topjohnwu@gmail.com>
2018-08-12 18:31:18 +0800

    add swapoff/swapon syscalls for android
    Auto generated by ndk-box-kitchen

--- libbb/missing_syscalls.c
+++ libbb/missing_syscalls.c
@@ -39,6 +39,16 @@ int pivot_root(const char *new_root, const char *put_old)
 	return syscall(__NR_pivot_root, new_root, put_old);
 }
 
+int swapoff(const char *path)
+{
+  return syscall(__NR_swapoff, path);
+}
+
+int swapon(const char *path, int swapflags)
+{
+  return syscall(__NR_swapon, path, swapflags);
+}
+
 # if __ANDROID_API__ < 21
 int tcdrain(int fd)
 {
