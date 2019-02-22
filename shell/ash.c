38523bea42fdd63746587015ddb8f2edfdb0f6dd
Chris Renshaw <osm0sis@outlook.com>
2019-02-22 13:01:48 -0400

    ash history
    allows ash history to work on Android
    
    Patch modified by Tias Guns <tias@ulyssis.org> from 'busybox-android.patch' by Alexandre Courbot
    https://github.com/Gnurou/busybox-android
    Rebased for busybox 1.30.1 by Chris Renshaw <osm0sis@outlook.com>

--- shell/ash.c
+++ shell/ash.c
@@ -14279,7 +14279,7 @@ procargs(char **argv)
 	int login_sh;
 
 	xargv = argv;
-	login_sh = xargv[0] && xargv[0][0] == '-';
+	login_sh = 1; /* = xargv[0] && xargv[0][0] == '-'; - make always true for Android */
 #if NUM_SCRIPTS > 0
 	if (minusc)
 		goto setarg0;
@@ -14444,7 +14444,7 @@ int ash_main(int argc UNUSED_PARAM, char **argv)
 		const char *hp;
 
 		state = 1;
-		read_profile("/etc/profile");
+		read_profile("/system/etc/profile");
  state1:
 		state = 2;
 		hp = lookupvar("HOME");
@@ -14489,6 +14489,9 @@ int ash_main(int argc UNUSED_PARAM, char **argv)
 		if (iflag) {
 			const char *hp = lookupvar("HISTFILE");
 			if (!hp) {
+#ifdef __ANDROID__
+				setvar("HISTFILE", "/mnt/sdcard/ash_history", 0);
+#else
 				hp = lookupvar("HOME");
 				if (hp) {
 					INT_OFF;
@@ -14498,6 +14501,7 @@ int ash_main(int argc UNUSED_PARAM, char **argv)
 					INT_ON;
 					hp = lookupvar("HISTFILE");
 				}
+#endif
 			}
 			if (hp)
 				line_input_state->hist_file = hp;
