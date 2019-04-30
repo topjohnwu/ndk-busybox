a57ccd7378e6858f9b2a564c75517ed4b41fcd39
topjohnwu <topjohnwu@gmail.com>
2019-04-30 01:01:26 -0400

    Add generated files for ndk-build
    Auto generated by ndk-box-kitchen

---
+++ Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+VERSION = 1
+PATCHLEVEL = 30
+SUBLEVEL = 1
+EXTRAVERSION = -topjohnwu
+BB_VER = $(VERSION).$(PATCHLEVEL).$(SUBLEVEL)$(EXTRAVERSION)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := busybox
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+include $(LOCAL_PATH)/Android_src.mk
+
+LOCAL_LDFLAGS := -static
+LOCAL_CFLAGS := \
+-w -include include/autoconf.h -D__USE_BSD \
+-DBB_VER=\"$(BB_VER)\" -DBB_BT=AUTOCONF_TIMESTAMP
+LOCAL_DISABLE_FORMAT_STRING_CHECKS := true
+
+include $(BUILD_EXECUTABLE)
